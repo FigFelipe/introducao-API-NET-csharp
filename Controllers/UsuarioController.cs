496616415a18aaeb874c4fb98e5234a1204ccc8f
Felipe Figueiredo <felipe_figueiredo@outlook.com.br>
2024-08-29 21:59:25 -0300

    ADD: Controller Usuario
    Controller Usuario retorna a data e hora atuais (do momento da requisição).

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationNETCore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet("ObterDataHoraAtual")]
+        public IActionResult ObterDataHora()
+        {
+            var obj = new
+            {
+                Data = DateTime.Now.ToLongDateString(),
+                Hora = DateTime.Now.ToShortTimeString()
+            };
+
+            return Ok(obj);
+        }
+    }
+}
