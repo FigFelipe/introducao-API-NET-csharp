bf4ded2992c112e1d32c44cd223cdd165f99bcd7
Felipe Figueiredo <felipe_figueiredo@outlook.com.br>
2024-09-04 21:49:12 -0300

    ADD: parametro JSON 'EnviaEmail'
    - appsettings.json: Ambiente de produção
    - appsettings.Development.json: Ambiente de testes

---
+++ Context/AgendaContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplicationNETCore.Entities;
+
+namespace WebApplicationNETCore.Context
+{
+    // Herdar os métodos da classe 'DbContext'
+    public class AgendaContext : DbContext
+    {
+        // Recebe a conexão do banco
+        // O método deve permanecer vazio
+        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options)
+        {
+
+        }
+
+        // Propriedade que refere-se á entidade (classe no programa e tabela no db)
+        public DbSet<Contato> Contatos { get; set; }
+    }
+}
---
+++ Entities/Contato.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplicationNETCore.Entities
+{
+    public class Contato
+    {
+        // Essa classe irá se transformar em uma tabela no banco de dados
+        // É um schema (modelo)
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
